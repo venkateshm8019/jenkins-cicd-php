95e4d690d5e520bd04ab332306a72b617d639f93
venkatesh <130450049+venkateshm8019@users.noreply.github.com>
2023-07-27 08:44:36 +0530

    Update index.php
    "changes"

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<html>
+ <html>
  <head>
-  <title>Index</title>
+ <title>Index</title>
  </head>
  <body>
  <?php echo '<p>Hello world from PHP! free style project!!venkatesh!</p>'; ?> 
  </body>
-</html>
+ </html>
